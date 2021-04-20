9cc393a67f6583890f0b186897e9a10a2bf3f170
Chris Rettig <jcrettig@fastmail.fm>
2021-04-20 06:00:36 -0400

    clean up of JS

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -3,68 +3,14 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-  console.log(data.features)                                                                //DELETE
-  // console.log(data.features[1].geometry.coordinates[2])                                     //DELETE
-  // console.log(data.features[1].properties.mag)                                              //DELETE
+  //Send data.features object to createFeatures function
+  createFeatures(data.features);  
 });
 
 function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-
-    // layer.bindPopup("<h3>" + feature.properties.place +                                  //DELETE
-    //   "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");                      //DELETE
-
-    //conditionals for earthquate magnitude
-    // for (var i = 0; i < feature.properties.place.length; i++) {
-    //   var color = ""
-    //   if (feature.properties.mag <= 1) {
-    //     color = "green"
-    //   }
-    //   else if (feature.properties.mag <= 3) {
-    //     color = "yellow"
-    //   }
-    //   else {
-    //     color = "red"
-    //   }
-    // }
-    var color = "red"
-    var circleRadius = feature.geometry.coordinates[2] * 10000
-    console.log(circleRadius)
-
-    // Add Circle to map
-    L.circleMarker(feature.properties.place, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillcolor: color,
-
-      //Adjust radius
-      radius: circleRadius
-    }).bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-
-    // Add Legend
-    // p = colorFactor(palette = c("green", "yellow", "red"), domain = c("<=10", "10-30", "30+"), ordered = T)
-
-    // addLegend(
-    //   position = "bottomright",
-    //   pal = p,      
-    //   values = ("<=1", "1-3", "3+")
-    // )
-
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  // var earthquakes = L.geoJSON(earthquakeData, {
-  //   onEachFeature: onEachFeature,
-  //   pointToLayer: function(feature,latlng)
-  // });
-
+    //Create circle colors based on depth of earthquake
+ 
   d3.json(queryUrl, function (data) {
 
     function getColor(Depth) {
@@ -81,6 +27,7 @@ function createFeatures(earthquakeData) {
       return color;
     }
 
+    //Create circle size based on magnitude of earthquakes
     function getRadius(magnitude) {
       if (magnitude === 0) {
         return 1;
@@ -88,25 +35,27 @@ function createFeatures(earthquakeData) {
       return magnitude * 5;
     }
 
+    // Create a GeoJSON layer 
     var earthquakes = L.geoJson(data, {
-      // We turn each feature into a circleMarker on the map.
+      // Turn features into  circleMarkers on the map.
       pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng);
       },
-      // We set the style for each circleMarker using our styleInfo function.
+
+      // Set style for circleMarkers
       style: function (feature, layer) {
         return {
           opacity: 1,
           fillOpacity: 1,
           fillColor: getColor(feature.geometry.coordinates[2]),
-          color: "#1c1c1c",
-          // radius: 10,
+          color: "#1c1c1c",          
           radius: getRadius(feature.properties.mag),
           stroke: true,
           weight: 0.5
         };
       },
-      // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+
+      // Create popups for each marker that display  magnitude depth and location of the earthquakes 
       onEachFeature: function (feature, layer) {
         layer.bindPopup(
           "Magnitude: "
@@ -118,10 +67,8 @@ function createFeatures(earthquakeData) {
         );
       }
     })
-    // .addTo(myMap);
-
-
-    // Sending our earthquakes layer to the createMap function
+    
+    // Send earthquakes layer to createMap function
     createMap(earthquakes);
   });
 }
@@ -144,18 +91,18 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  // Define a baseMaps object to hold our base layers
+  // Define baseMaps object to hold base layers
   var baseMaps = {
     "Street Map": streetmap,
     "Dark Map": darkmap
   };
 
-  // Create overlay object to hold our overlay layer
+  // Create overlay object to hold overlay layer
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create map, giving it streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
@@ -163,10 +110,12 @@ function createMap(earthquakes) {
     zoom: 5,
     layers: [streetmap, earthquakes]
   });
-  // Here we create a legend control object.
+
+  // Create legend control object.
   var legend = L.control({
     position: "bottomleft"
   });
+
   legend.onAdd = function () {
     var div = L.DomUtil.create("div", "info legend");
     var grades = [0, 7, 14];
@@ -174,19 +123,20 @@ function createMap(earthquakes) {
       "#72fa41",
       "#fbff00",
       "#eb3434"];
+
     // Loop through our intervals and generate a label with a colored square for each interval.
     for (var i = 0; i < grades.length; i++) {
-      div.innerHTML += "<div style='margin-top: 10px;'><i style='background: " + colors[i] + "'></i> " + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "</div><br>" : "+");
-      //<i style='background: "#72fa41"'></i>0-7<br>
+      div.innerHTML += "<div style='margin-top: 10px;'><i style='background: " + colors[i] + "'></i> " + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "</div><br>" : "+");     
     }
     return div;
   };
-  // We add our legend to the map.
+
+  //Add legend to map.
   legend.addTo(myMap);
 
   // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
+  // Pass in baseMaps and overlayMaps
+  // Add layer control to map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
